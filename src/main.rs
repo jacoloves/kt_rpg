7d14e7d709934e512b4825e4d9209b22ca8068c2
shooonng <5511068t@gmail.com>
2024-07-09 10:48:24 +0900

    add: charcter monster status

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize, Debug)]
+struct Character {
+    name: String,
+    lv: u32,
+    hp: i32,
+    min_attack: i32,
+    max_attack: i32,
+    min_recovery: i32,
+    max_recovery: i32,
+    exp: u32,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Monster {
+    name: String,
+    hp: i32,
+    min_attack: i32,
+    max_attack: i32,
+    exp: u32,
+}
+
 fn main() {
     println!("Hello, world!");
 }
